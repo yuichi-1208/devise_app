be35110ccd030f2a1ad3d37ddc00a78732a327c5
yuichi oyama <yuichi19991208@gmail.com>
2020-10-30 12:12:53 +0900

    deviseとletter_openerのgemを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
