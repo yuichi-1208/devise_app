ef8bffc749ef6cd9b99f98c97e262a4a067149e0
yuichi oyama <yuichi19991208@gmail.com>
2020-10-30 13:32:24 +0900

    deviseのhelperメソッドを定義

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create]
 
   # GET /blogs
   # GET /blogs.json
